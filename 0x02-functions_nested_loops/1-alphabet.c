bdacc6b21eb474a342cd4193e46fc7ab08fcdcb9
Langi <abdulrahman.omboke@gmail.com>
2022-12-14 06:35:19 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet in lowercase
+ * Return: Always 0
+ */
+void main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
