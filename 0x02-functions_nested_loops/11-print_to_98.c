5cd64cada769e94806db550e0a8f1ca660aa44e4
Langi <abdulrahman.omboke@gmail.com>
2022-12-14 18:46:44 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
@@ -17,5 +17,6 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 			printf("%d\n", n++);
+		printf("%d\n", n);
 	}
 }
