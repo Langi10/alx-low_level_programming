3cca4d269b493191c2558fc667b34f921b260661
Langi <abdulrahman.omboke@gmail.com>
2022-12-16 01:21:18 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,15 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (b = 0; b < 10, b++)
+	for (a = 0; a <= 10, a++)
 	{
-		for (a = 0; a <= 14; a++)
+		for (b = 0; b <= 14; b++)
 		{
-			if (a >= 10)
+			if (b >= 10)
 			{
-				_putchar((a / 10) + '0');
+				_putchar('1');
 			}
-			_putchar((a % 10) + '0');
+			_putchar((b % 10) + '0');
 		}
 		_putchar('\n');
 	}
