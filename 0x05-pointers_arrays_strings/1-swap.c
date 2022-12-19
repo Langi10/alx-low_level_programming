b87bf61e7502c3ee5ff6d3fede45790a296f6dcd
Langi <abdulrahman.omboke@gmail.com>
2022-12-19 09:47:38 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#inlcude "main.h"
+
+/**
+ * swap_int - a program that swaps the values of two integers
+ * @a: parameter
+ * @b: parameter
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int r = *a;
+
+	*a = *b;
+	*b = r;
+}
