3b1a4d6e7ca17864e97bd28e40902e3af08c4902
Langi <abdulrahman.omboke@gmail.com>
2023-01-13 02:13:47 -0800

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,30 +2,37 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - a function that concatinates two strings
- * @s1: newly allocated memory pointer
- * @s2: second string
- * @n: amount of bytes
+ * string_nconcat - a function that concatenates two strings
+ * @s1: newly allocated space for string
+ * @s2: string
+ * @n: number of bytes
+ *
  * Return: NULL if it fails,s2 if n>s2,
  * if NULL is passed, treat it as an empty string
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *sout;
 	unsigned int ls1, ls2, lsout, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	for (ls1 = 0, s1[ls1] != '\0', ls1++)
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
 		;
-	for (ls2 = 0, s2[ls2] != '\0', ls2++)
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
 		;
+
 	if (n > ls2)
 		n = ls2;
+
 	lsout = ls1 + n;
+
 	sout = malloc(lsout + 1);
 
 	if (sout == NULL)
@@ -35,7 +42,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 		if (i < ls1)
 			sout[i] = s1[i];
 		else
-			sout[i] = s1[i];
-		sout[i] = '\0';
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
 	return (sout);
 }
